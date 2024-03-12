977cd09ce5a24ece1571b3c4f4f8d0190551e95d
Redouane Sarouf <red.sarouf2014@gmail.com>
2024-03-12 13:42:00 -0400

    solve fibonacci sequence

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
   // type your code here
+  if(num == 0 || num == 1){
+    return num;
+  } else {
+    return fibonacci(num -1) + fibonacci( num -2);
+  }
 }
 
 if (require.main === module) {
