004ca79ff9ba2ccfd44cf4b9005dbbd965df63d5
Redouane Sarouf <red.sarouf2014@gmail.com>
2024-03-12 12:36:22 -0400

    solve reverse string algorithm

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,7 @@
 function reverseString(str) {
   // type your code here
+  const newStr = str.split('').reverse().join('');
+  return newStr;
 }
 
 if (require.main === module) {
@@ -17,3 +19,4 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+reverseString('hi');
