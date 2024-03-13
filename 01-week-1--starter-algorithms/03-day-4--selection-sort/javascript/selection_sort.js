884913371c7ddcb43c178e0a2455263cc6f2f3a3
Redouane Sarouf <red.sarouf2014@gmail.com>
2024-03-12 23:05:34 -0400

    solve selection sort

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,7 +1,33 @@
 function selectionSort(arr) {
   // type your code here
+
+ 
+    
+
+  for(let i = arr.length - 1; i > 0; i--){
+    let largest = 0;
+    for(let j = 1; j <= i; j++){
+      if(arr[j] > arr[largest]){
+        largest = j;
+      }
+    }
+    swap(arr, largest, i);
+
+      
+  }
+  return arr;
+
 }
 
+function swap(intArr, i, j){
+  if(i==j){
+    return;
+  }
+  let temp = intArr[i];
+  intArr[i]=intArr[j];
+  intArr[j]=temp;
+}
+console.log(selectionSort([15, 2, 5,3,5]))
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
